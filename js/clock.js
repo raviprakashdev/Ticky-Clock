c5535cdef39bd39a3b3e9c091fc6a481252addc8
Ravi Prakash <raviprakash01a@gmail.com>
2019-01-26 00:42:33 +0530

    first commit

---
+++ js/clock.js
@@ -0,0 +1,32 @@
+var d, h, m, s, clock, clr,tt;
+
+function cloky(){
+    d = new Date();
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+
+if( h<=9){
+    h='0' + h;
+    
+}
+if( m<=9){
+    m='0' + m;
+
+}
+if( s<=9){
+    s='0' + s;
+}
+
+clock = h+":"+m+":"+s;
+clr = "#" + m+s+h;
+tt=s * 1.33+"%";
+document.getElementById("clock").innerHTML=clock,
+document.body.style.backgroundColor=clr;
+
+document.getElementById("clock").style.position="relative";
+document.getElementById("clock").style.top=tt;
+
+setTimeout(cloky,1000);
+}
+cloky();
