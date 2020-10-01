c095b732875f0a9ca89cf1ee8a627e0b2b347bce
arnoldtherigan15 <arnoldtherigan15@gmail.com>
2020-10-01 21:40:30 +0700

    add random clock background

--- js/clock.js
+++ js/clock.js
@@ -20,9 +20,11 @@ if( s<=9){
 
 clock = h+":"+m+":"+s;
 clr = "#" + m+s+h;
+clr_clock_text = "#" + s+h+m;
 tt=s * 1.33+"%";
 document.getElementById("clock").innerHTML=clock,
 document.body.style.backgroundColor=clr;
+document.getElementById("clock").style.backgroundColor=clr_clock_text;
 
 document.getElementById("clock").style.position="relative";
 document.getElementById("clock").style.top=tt;
